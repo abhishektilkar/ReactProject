f6d94aa86e670d0fdf195f548447c6037ea43876
abhishektilkar <tilkarabhishek2000@gmail.com>
2024-06-11 00:13:54 +0530

    HomePage UI Compelete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+// import HomeCards from './component/HomeCards';
+import Hero from './component/Hero';
+import HomeCards from './component/HomeCards';
+import JobListings from './component/JobListings';
 import Navbar from './component/Navbar'
+import ViewAllJobs from './component/ViewAllJobs';
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar/>
-    </div>
+      <Hero/>
+      <HomeCards/>
+      <JobListings/>
+      <ViewAllJobs/>
+    </>
   )
 }
 
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
         <div className="flex h-20 items-center justify-between">
           <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             
-            <a className="flex flex-shrink-0 items-center mr-4" href="/index.html">
+            <a className="flex flex-shrink-0 items-center mr-4" href="/">
               
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#a7d629" className="h-10 w-auto">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" className="h-10 w-auto">
                 <path fillRule="evenodd" d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z" clipRule="evenodd" />
               </svg>
 
